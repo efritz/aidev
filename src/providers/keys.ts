f5c82ea45fc157487db17689deab3431e30176ed
Eric Fritz <eric@eric-fritz.com>
2024-08-15 16:30:42 -0500

    Update keys path.

--- src/providers/keys.ts
+++ src/providers/keys.ts
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs'
 import { homedir } from 'os'
 import path from 'path'
 
-const SCRIPTS_ROOT = path.join(homedir(), '.dotfiles', 'ai', 'scripts')
+const SCRIPTS_ROOT = path.join(homedir(), 'dev', 'efritz', 'aidev')
 
 export function getKey(name: string) {
     return readFileSync(path.join(SCRIPTS_ROOT, 'keys', `${name}.key`), 'utf8').trim()
