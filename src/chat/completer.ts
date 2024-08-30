f2d52e32c86aa96513cfc28229281d3ec2d0e3cb
Eric Fritz <eric@eric-fritz.com>
2024-08-29 23:34:57 -0500

    Remove @file completions.

--- src/chat/completer.ts
+++ src/chat/completer.ts
@@ -1,6 +1,4 @@
 import { CompleterResult } from 'readline'
-import { expandFilePatterns } from '../util/fs/glob'
-import { filterIgnoredPaths } from '../util/fs/ignore'
 import { commands, completeCommand } from './commands/commands'
 import { ChatContext } from './context'
 
@@ -24,31 +22,6 @@ export function completer(context: ChatContext, line: string): CompleterResult {
         return [hits, line]
     }
 
-    // Complete a @tagged-file at the end of the input
-    const taggedFileResult = completeTaggedFile(line)
-    if (taggedFileResult) {
-        return taggedFileResult
-    }
-
     // No completion suggestions
     return [[], line]
-}
-
-export function completeTaggedFile(line: string): CompleterResult | undefined {
-    const parts = line.split(' ')
-    const suffix = parts[parts.length - 1]
-
-    if (!suffix.startsWith('@')) {
-        return undefined
-    }
-
-    const searchTerm = suffix.slice(1).toLowerCase()
-    const allFiles = filterIgnoredPaths(expandFilePatterns(['./**/*']), true)
-    const matchingFiles = allFiles.filter(file => file.toLowerCase().includes(searchTerm))
-
-    if (matchingFiles.length > 0) {
-        return [matchingFiles.map(file => '@' + file + (matchingFiles.length === 1 ? ' ' : '')), suffix]
-    }
-
-    return undefined
 }
