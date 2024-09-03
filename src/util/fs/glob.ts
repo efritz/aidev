eb06a2ba3860319b1cff402a00822115de5eb384
Eric Fritz <eric@eric-fritz.com>
2024-09-03 01:27:50 -0500

    Asyncify glob functions.

--- src/util/fs/glob.ts
+++ src/util/fs/glob.ts
@@ -1,36 +1,35 @@
-import { lstatSync } from 'fs'
+import { lstat } from 'fs/promises'
 import { sep } from 'path'
 import { glob } from 'glob'
 
 export async function expandFilePatterns(patterns: string[]): Promise<string[]> {
-    return patterns.flatMap(pattern => {
+    const expanded: string[] = []
+    for (const pattern of patterns) {
         if (pattern.includes('*')) {
-            return glob.sync(pattern, { nodir: true })
+            expanded.push(...(await glob.glob(pattern, { nodir: true })))
+        } else if (!(await isDir(pattern))) {
+            expanded.push(pattern)
         }
+    }
 
-        if (!isDir(pattern)) {
-            return [pattern]
-        }
-
-        return []
-    })
+    return expanded
 }
 
 export async function expandDirectoryPatterns(patterns: string[]): Promise<string[]> {
-    return patterns.flatMap(pattern => {
+    const expanded: string[] = []
+    for (const pattern of patterns) {
         if (pattern.includes('*')) {
-            return glob
-                .sync(pattern, { withFileTypes: true })
-                .filter(entry => entry.isDirectory())
-                .map(r => r.relativePosix() + sep)
+            expanded.push(
+                ...(await glob.glob(pattern, { withFileTypes: true }))
+                    .filter(entry => entry.isDirectory())
+                    .map(r => r.relativePosix() + sep),
+            )
+        } else if (await isDir(pattern)) {
+            expanded.push(pattern)
         }
+    }
 
-        if (isDir(pattern)) {
-            return [pattern]
-        }
-
-        return []
-    })
+    return expanded
 }
 
 // Expand the given path prefixes to all _immediate_ descendants that match the prefix.
@@ -38,20 +37,29 @@ export async function expandDirectoryPatterns(patterns: string[]): Promise<strin
 // This assumes that none of the given prefixes already contain wildcards. If so, they
 // should be expanded independently.
 export async function expandPrefixes(prefixes: string[]): Promise<string[]> {
-    return prefixes.flatMap(prefix =>
-        glob
-            .sync(prefix + '*')
-            // Add a trailing slash to directories
-            .map(path => `${path}${isDir(path) ? sep : ''}`)
-            // Do not complete directories to themselves
-            .filter(path => !(path.endsWith(sep) && path === prefix)),
-    )
+    const expanded: string[] = []
+    for (const prefix of prefixes) {
+        for (let path of await glob.glob(prefix + '*')) {
+            if (await isDir(path)) {
+                // Add a trailing slash to directories
+                path += sep
+
+                if (path === prefix) {
+                    // Do not complete directories to themselves
+                    continue
+                }
+            }
+
+            expanded.push(path)
+        }
+    }
+
+    return expanded
 }
 
-function isDir(path: string): boolean {
+async function isDir(path: string): Promise<boolean> {
     try {
-        const stat = lstatSync(path)
-        return stat.isDirectory()
+        return (await lstat(path)).isDirectory()
     } catch (error: any) {
         return false
     }
