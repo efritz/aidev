cd624b1f4d6969d12f3cabb90426e987108c3c0f
Eric Fritz <eric@eric-fritz.com>
2024-10-14 13:39:03 -0500

    Improve completion ux.

--- src/util/fs/completion.ts
+++ src/util/fs/completion.ts
@@ -84,8 +84,19 @@ async function completePathPatterns(
     args: string,
     expandPatterns: (patterns: string[]) => Promise<string[]>,
 ): Promise<CompleterResult> {
-    const parsedArgs = parseArgsWithEscapedSpaces(args, true)
-    const last = parsedArgs[parsedArgs.length - 1] || ''
+    // Get the _raw_ last element of the argument list including any quotes and
+    // escapes. We need this to be verbatim so that the completer can replace part
+    // of the input string. We'll need to de-quote and un-escape the quotes for
+    // processing later.
+    //
+    // We'll skip trying to complete an element that is already terminated by an
+    // unescaped space, as it's likely the user is trying to start a new element.
+    const lastElementTerminated = args.endsWith(' ') && !args.endsWith('\\ ')
+    const last = (!lastElementTerminated && parseArgsWithEscapedSpaces(args, true).pop()) || ''
+
+    // Canonicalize the last element into a path prefix we know how to expand.
+    // This de-quotes and un-escapes spaces, as well as normalizes things like
+    // home directory references and relative paths.
     const prefix = canonicalizePathPrefix(last)
 
     if (prefix.includes('*')) {
@@ -103,6 +114,13 @@ async function completePathPatterns(
 
     const expandedPrefixes = await expandPrefixes([prefix])
     const quotedPrefixes = expandedPrefixes.map(entry => entry.replaceAll(/([^\\]) /g, '$1\\ '))
+
+    // If there is only one match and it's not a directory, add a trailing space. This will
+    // replace the entry with the only candidate path and set the user up for a new argument.
+    if (quotedPrefixes.length === 1 && !quotedPrefixes[0].endsWith('/')) {
+        return [[quotedPrefixes[0] + ' '], last]
+    }
+
     return [quotedPrefixes, last]
 }
 
