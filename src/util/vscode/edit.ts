3db778c6c5f9437e85ef3becad63ef439c25869c
Eric Fritz <eric@eric-fritz.com>
2024-10-21 23:34:03 -0500

    WIP.

--- src/util/vscode/edit.ts
+++ src/util/vscode/edit.ts
@@ -4,8 +4,12 @@ import { withTempFileContents } from '../fs/temp'
 import { withFileWatcher } from '../fs/watch'
 import { CancelError, InterruptHandler } from '../interrupts/interrupts'
 
-export function withContentEditor(interruptHandler: InterruptHandler, contents: string): Promise<string> {
-    return withEditorOverTempFile(interruptHandler, contents, tempPath => [tempPath])
+export function withContentEditor(
+    interruptHandler: InterruptHandler,
+    contents: string,
+    referencePath: string = '',
+): Promise<string> {
+    return withEditorOverTempFile(interruptHandler, contents, tempPath => [tempPath], referencePath)
 }
 
 export function withDiffEditor(
@@ -13,34 +17,43 @@ export function withDiffEditor(
     originalPath: string,
     contents: string,
 ): Promise<string> {
-    return withEditorOverTempFile(interruptHandler, contents, tempPath => ['--diff', originalPath, tempPath])
+    return withEditorOverTempFile(
+        interruptHandler,
+        contents,
+        tempPath => ['--diff', originalPath, tempPath],
+        originalPath,
+    )
 }
 
 function withEditorOverTempFile(
     interruptHandler: InterruptHandler,
     contents: string,
     args: (tempPath: string) => string[],
+    referencePath: string = '',
 ): Promise<string> {
-    return withTempFileContents(contents, tempPath =>
-        withFileWatcher(tempPath, watcher =>
-            interruptHandler.withInterruptHandler(
-                () =>
-                    new Promise<string>((resolve, reject) => {
-                        watcher.on('change', () => {
-                            readFile(tempPath, 'utf-8')
-                                .then(newContent => {
-                                    if (newContent !== contents) {
-                                        resolve(newContent)
-                                    }
-                                })
-                                .catch(() => {})
-                        })
+    return withTempFileContents(
+        contents,
+        tempPath =>
+            withFileWatcher(tempPath, watcher =>
+                interruptHandler.withInterruptHandler(
+                    () =>
+                        new Promise<string>((resolve, reject) => {
+                            watcher.on('change', () => {
+                                readFile(tempPath, 'utf-8')
+                                    .then(newContent => {
+                                        if (newContent !== contents) {
+                                            resolve(newContent)
+                                        }
+                                    })
+                                    .catch(() => {})
+                            })
 
-                        spawn('code', [...args(tempPath), '--wait'])
-                            .on('exit', () => reject(new CancelError('User canceled')))
-                            .on('error', error => reject(new Error(`Failed to open editor: ${error.message}`)))
-                    }),
+                            spawn('code', [...args(tempPath), '--wait'])
+                                .on('exit', () => reject(new CancelError('User canceled')))
+                                .on('error', error => reject(new Error(`Failed to open editor: ${error.message}`)))
+                        }),
+                ),
             ),
-        ),
+        referencePath,
     )
 }
