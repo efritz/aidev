670ac80c75d96fda6ddce65e29ba607414f7e921
Eric Fritz <eric@eric-fritz.com>
2025-02-05 09:17:54 -0600

    Read empty string for files > 1MB.

--- src/util/fs/safe.ts
+++ src/util/fs/safe.ts
@@ -18,12 +18,16 @@ export async function isDir(path: string): Promise<boolean> {
     }
 }
 
+const maxSize = 1_000_000 // 1MB
+
 export async function safeReadFile(path: string): Promise<string> {
     try {
-        const content = await readFile(path, 'utf-8')
+        if ((await lstat(path)).size <= maxSize) {
+            const content = await readFile(path, 'utf-8')
 
-        if (!(await isBinaryFile(Buffer.from(content)))) {
-            return content
+            if (!(await isBinaryFile(Buffer.from(content)))) {
+                return content
+            }
         }
     } catch (error: any) {
         // swallow
